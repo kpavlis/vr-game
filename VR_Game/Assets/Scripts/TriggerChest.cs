f4860a878497a84e4d346b58e976c220f54a1416
PavlisKostas <98098077+PavlisKostas@users.noreply.github.com>
2025-01-26 19:42:45 +0200

    Updates to Animations

---
+++ VR_Game/Assets/Scripts/TriggerChest.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TriggerChest : MonoBehaviour
+{
+    public GameObject mText;
+    public GameObject mBackground;
+    private TextMeshProUGUI textVariable;
+    private Animator mAnimator;
+
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+        textVariable = mText.GetComponent<TextMeshProUGUI>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            mAnimator.SetTrigger("IsHere");
+            mBackground.SetActive(true);
+            textVariable.color = Color.black;
+            textVariable.text = "Καλωσήρθες στον θησαυρό ! Ανακάλυψε τα μυστικά...";
+            mText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            mAnimator.SetTrigger("IsAway");
+            textVariable.text = "";
+            mBackground.SetActive(false);
+            textVariable.color = Color.white;
+            mText.SetActive(false);
+        }
+    }
+}
